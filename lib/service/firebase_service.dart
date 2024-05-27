d65937f651f71bbabc9953dbb54513d32cfe4317
alvaroo611 <alvaromarmolromero@gmail.com>
2024-05-27 13:24:57 +0200

    prueba

--- lib/service/firebase_service.dart
+++ lib/service/firebase_service.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
+//Class firebase
 class FirebaseService {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = kIsWeb
