4bae60ce57c9e86d3ccb68186547ccf23a034ad8
alvaroo611 <alvaromarmolromero@gmail.com>
2024-05-28 15:35:08 +0200

    deploy

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:iseneca/screens/screens.dart';
 import 'package:iseneca/service/services.dart';
 import 'package:provider/provider.dart';
 import 'package:iseneca/providers/profesores_provider.dart';
+
 //Main
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,7 +40,8 @@ void logOut() async {
     }
   }
 }
-//AppState
+
+//AppState class
 class AppState extends StatelessWidget {
   const AppState({super.key});
 
