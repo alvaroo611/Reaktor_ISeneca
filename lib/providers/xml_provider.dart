b8ee7c7cb06b32c818d97908f471d4ca002c4f4b
alvaroo611 <alvaromarmolromero@gmail.com>
2024-06-03 13:58:24 +0200

    deploy2

--- lib/providers/xml_provider.dart
+++ lib/providers/xml_provider.dart
@@ -27,7 +27,7 @@ class XmlProvider extends ChangeNotifier {
 
       // Enviar la solicitud HTTP con Dio
       Response response = await _dio.post(
-        'http://localhost:8088/horarios/send/xml',
+        'http://microservices-iesjandula.duckdns.org:8088/horarios/send/xml',
         data: formData,
         options: Options(
           headers: {
