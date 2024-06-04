04ec259c104b645cc8ae85a0c1f707d718528ec2
alvaroo611 <alvaromarmolromero@gmail.com>
2024-06-04 11:07:27 +0200

    commit

--- lib/providers/credenciales_provider.dart
+++ lib/providers/credenciales_provider.dart
@@ -17,7 +17,8 @@ class CredencialesProvider extends ChangeNotifier {
   }
 
   getCredencialesUsuario() async {
-    const url = "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=1qREuUYht73nx_fS2dxm9m6qPs_uvBwsK74dOprmwdjE&sheet=Credenciales"; 
+    const url =
+        "https://script.google.com/macros/s/AKfycbyPsB_koj3MwkmRFn8IJU-k4sOP8nRfnHHKNNt9xov9INZ1VEsQbu96gDR8Seiz0oDGOQ/exec?spreadsheetId=1qREuUYht73nx_fS2dxm9m6qPs_uvBwsK74dOprmwdjE&sheet=Credenciales";
     String respuesta = await Utilidades.getJsonData(url);
     respuesta = '{"results":$respuesta}';
     final credencialesResponse = CredencialesResponse.fromJson(respuesta);
